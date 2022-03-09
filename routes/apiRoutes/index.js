a1fdeec6fa93347437219a8603a1f0c9dd065410
KatyKedi <katykat5bros@gmail.com>
2022-03-09 15:43:29 -0800

    user can add a note to the list

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const db = require('../../Develop/db/db');
+const fs = require('fs');
+const path = require('path');
 
 function filterByQuery(query, notesArray) {
     let filteredResults = notesArray;
@@ -12,12 +14,44 @@ function filterByQuery(query, notesArray) {
     return filteredResults;
 };
 
+function findById(id, notesArray) {
+    const result = notesArray.filter(note => note.id === id)[0];
+    return result;
+};
+
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, '../../Develop/db/db.json'),
+        JSON.stringify(notesArray, null, 2)
+    );
+    return note;
+};
+
 router.get('/notes', (req, res) => {
     let results = db;
     if (req.query) {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
+});
+
+router.get('/notes/:id', (req, res) => {
+    const result = findById(req.params.id, db);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+});
+
+router.post('/notes', (req, res) => {
+    // set Id based on what the next index of the array will be
+    req.body.id = db.length.toString();
+    // add note to json file and notes array in this function
+    const note = createNewNote(req.body, db);
+    res.json(note);
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes/index.js');
+const htmlRoutes = require('./routes/htmlRoutes/index.js');
 
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true}))
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true}))
 app.use(express.json());
 
 // make certain files readily available and not gate it behind a server endpoint
-app.use(express.static('public'));
+app.use(express.static('Develop/public'));
 
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
